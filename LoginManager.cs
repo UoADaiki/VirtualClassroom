cac34a1032fcc583f58b75ff769a14e529fd2fe7
UoADaiki <52566133+UoADaiki@users.noreply.github.com>
2021-07-05 19:47:43 +0900

    Add files via upload

---
+++ LoginManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{   
+    // Setting gameobjects from the inspecter window
+    [SerializeField] GameObject startPanel;
+    [SerializeField] GameObject createPanel;
+
+    public static bool isTeacher;  
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartMenu();
+        isTeacher = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // The process when BackButton is pushed
+    public void StartMenu(){
+        startPanel.SetActive(true);
+        createPanel.SetActive(false);
+    }
+
+    // The process when CreateButton is pushed at CreatePanel or JoinPanel 
+    public void CreateMenu(){
+        startPanel.SetActive(false);
+        createPanel.SetActive(true);
+    }
+
+    // The process when CreateButton is pushed at CreatePanel
+    public void CreateRoom(){
+        isTeacher = true;
+        SceneManager.LoadScene("Classroom");
+    }
+
+    // Send the user status(teacher or student) for Classroom scene
+    public static bool checkTeacher(){
+        return isTeacher;
+    }
+
+}
