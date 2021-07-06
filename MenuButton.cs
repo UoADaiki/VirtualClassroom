a19fc8522336b639af8daf82cc90a31d9c41625e
UoADaiki <52566133+UoADaiki@users.noreply.github.com>
2021-07-06 19:21:54 +0900

    Add files via upload

---
+++ MenuButton.cs
@@ -0,0 +1,37 @@
+﻿/*
+The manipulation of user menu button
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    GameObject menuPanel;
+    bool menuFlag;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        menuPanel = GameObject.Find("MenuPanel");
+        menuPanel.SetActive(false);
+        menuFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenuClick(){
+        if(menuFlag == true){   // When the menu is open,
+            menuPanel.SetActive(false);    // menu is close.
+            menuFlag = false;
+        }
+        else{                   // When the menu is close,
+            menuPanel.SetActive(true);      // menu is open.
+            menuFlag = true;
+        }
+    }
+}
