1d052fd8361ce70cbf1ef06fdb5731cc26c8fc93
UoADaiki <52566133+UoADaiki@users.noreply.github.com>
2021-10-12 22:23:14 +0900

    Add files via upload

---
+++ AvatarController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class AvatarController : MonoBehaviourPunCallbacks
+{
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(photonView.IsMine == true){
+
+            if (Input.GetKey ("up")) {
+			    transform.position += transform.forward * speed * Time.deltaTime;
+		    }
+		    if (Input.GetKey ("down")) {
+			    transform.position -= transform.forward * speed * Time.deltaTime;
+		    }
+		    if (Input.GetKey("right")) {
+			    //transform.position += transform.right * speed * Time.deltaTime;
+                transform.Rotate(0.0f, 1.0f, 0.0f);
+		    }
+		    if (Input.GetKey ("left")) {
+			    //transform.position -= transform.right * speed * Time.deltaTime;
+                transform.Rotate(0.0f, -1.0f, 0.0f);
+		    }
+        }   
+    }
+}
--- WebCamera.cs
+++ WebCamera.cs
@@ -5,50 +5,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class WebCamera : MonoBehaviour
+public class WebCamera : MonoBehaviourPunCallbacks
 {   
     // width and height are constant value
-    private int width = 1;
-    private int height = 1;
+    private int width = 256;
+    private int height = 256;
     private int fps = 40;  
     
     WebCamTexture webCamTexture;
-    [SerializeField] GameObject cube;  // Camera object
+    [SerializeField] Renderer cube;  // Camera object
     [SerializeField] Texture offCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         webCamTexture = new WebCamTexture(width, height, fps); // width, height, framerate
-        cube.GetComponent<MeshRenderer>().material.mainTexture = offCamera;
+        //cube.GetComponent<MeshRenderer>().material.mainTexture = offCamera;
+        cube.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.V)){        // When you press left Alt(option) + V key,
-            if(webCamTexture.isPlaying == false){  // if your camera is off,
-                cube.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
-                webCamTexture.Play();   // turn on your camera.
+    {   
+        if(photonView.IsMine == true){   
+            if(Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.V)){        // When you press left Alt(option) + V key,
+                photonView.RPC("OnOffCamera", RpcTarget.AllBuffered);
             }
-            else{                       // if your camera is on,
-                cube.GetComponent<MeshRenderer>().material.mainTexture = offCamera;
-                webCamTexture.Stop();   // turn off your camera.
-            }
+        }
+         
+    }
+
+    // Switch web camera ON/OFF 
+    [PunRPC]
+    public void OnOffCamera(){      
+        if(webCamTexture.isPlaying == false){  // if your camera is off,
+            cube.enabled = true;      
+            cube.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
+            webCamTexture.Play();   // turn on your camera.
+        }
+        else{                       // if your camera is on,
+            //cube.GetComponent<MeshRenderer>().material.mainTexture = offCamera;
+            webCamTexture.Stop();   // turn off your camera.
+            cube.enabled = false;
         }
         
     }
 
-    // The process when Camera ON/OFF button is pushed
-    public void OnOffCamera(){
-        if(webCamTexture.isPlaying == false){  // if your camera is off,
-                cube.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
-                webCamTexture.Play();   // turn on your camera.
-        }
-        else{                       // if your camera is on,
-                cube.GetComponent<MeshRenderer>().material.mainTexture = offCamera;
-                webCamTexture.Stop();   // turn off your camera.
+    public void cameraButton(){
+        if(photonView.IsMine == true){   
+            photonView.RPC("OnOffCamera", RpcTarget.AllBuffered);          
         }
     }
+
 }
